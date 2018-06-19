fff09047f591e7194987fe1f3542a46679913a80
lilyorchard <lily@orchardfamily.com>
2018-06-19 18:55:27 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,6 +1,10 @@
 def simple_curfew_checker(t)
   if t < 11
-    puts
+    puts "You are early!"
+  elsif t > 11
+    puts "Honey, you are late!"
+  else t == 11
+    puts "Run girl, RUUUUNNNN!"
 end
 
 def curfew_checker(time)
