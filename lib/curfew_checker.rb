51fdbe4e5935d4579c1beb328378783d67d5d31e
lilyorchard <lily@orchardfamily.com>
2018-06-19 19:54:31 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,44 +1,44 @@
-# # def simple_curfew_checker(t)
-# #   if t < 11
-# #     puts "You're not in trouble!"
-# #   else t >= 11
-# #     puts "You're in trouble! Better get home quick!"
-# #   end
-# # end
-# # simple_curfew_checker(10)
-
-# # def curfew_checker(t)
-# #   if t < 11
-# #     puts "Keep having fun!"
-# #   else t >= 11
-# #     puts "You're in trouble! Better get home quick!"
-# #   end
-# # end
-# # curfew_checker(12)
-
-
-# # def complex_curfew_checker(t)
-# #   if t < 11
-# #     puts "Keep having fun!"
-# #   elsif t > 11
-# #     puts "You're in trouble! Better get home quick!"
-# #   else t == 11
-# #     puts "Time to apparate!"
-# #   end
-# # end
-# # complex_curfew_checker(11)
-
-# def deluxe_curfew_checker(t)
+# def simple_curfew_checker(t)
 #   if t < 11
-#     r = 11-t
-#     puts "Keep having fun! You have #{r} hour(s) left."
+#     puts "You're not in trouble!"
+#   else t >= 11
+#     puts "You're in trouble! Better get home quick!"
+#   end
+# end
+# simple_curfew_checker(10)
+
+# def curfew_checker(t)
+#   if t < 11
+#     puts "Keep having fun!"
+#   else t >= 11
+#     puts "You're in trouble! Better get home quick!"
+#   end
+# end
+# curfew_checker(12)
+
+
+# def complex_curfew_checker(t)
+#   if t < 11
+#     puts "Keep having fun!"
 #   elsif t > 11
 #     puts "You're in trouble! Better get home quick!"
 #   else t == 11
 #     puts "Time to apparate!"
 #   end
 # end
-# deluxe_curfew_checker(11)
+# complex_curfew_checker(11)
+
+def deluxe_curfew_checker(t)
+  if t < 11
+    r = 11-t
+    puts "Keep having fun! You have #{r} hour(s) left."
+  elsif t > 11
+    puts "You're in trouble! Better get home quick!"
+  else t == 11
+    puts "Time to apparate!"
+  end
+end
+deluxe_curfew_checker(11)
 
 def platinum_curfew_checker(t, c)
   if t < 11
