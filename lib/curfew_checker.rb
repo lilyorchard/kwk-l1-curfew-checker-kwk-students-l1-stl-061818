e9c1f7d1dfa06de6f489f78565c815d417a91f77
lilyorchard <lily@orchardfamily.com>
2018-06-19 19:54:34 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,32 +1,32 @@
-# def simple_curfew_checker(t)
-#   if t < 11
-#     puts "You're not in trouble!"
-#   else t >= 11
-#     puts "You're in trouble! Better get home quick!"
-#   end
-# end
-# simple_curfew_checker(10)
+def simple_curfew_checker(t)
+  if t < 11
+    puts "You're not in trouble!"
+  else t >= 11
+    puts "You're in trouble! Better get home quick!"
+  end
+end
+simple_curfew_checker(10)
 
-# def curfew_checker(t)
-#   if t < 11
-#     puts "Keep having fun!"
-#   else t >= 11
-#     puts "You're in trouble! Better get home quick!"
-#   end
-# end
-# curfew_checker(12)
+def curfew_checker(t)
+  if t < 11
+    puts "Keep having fun!"
+  else t >= 11
+    puts "You're in trouble! Better get home quick!"
+  end
+end
+curfew_checker(12)
 
 
-# def complex_curfew_checker(t)
-#   if t < 11
-#     puts "Keep having fun!"
-#   elsif t > 11
-#     puts "You're in trouble! Better get home quick!"
-#   else t == 11
-#     puts "Time to apparate!"
-#   end
-# end
-# complex_curfew_checker(11)
+def complex_curfew_checker(t)
+  if t < 11
+    puts "Keep having fun!"
+  elsif t > 11
+    puts "You're in trouble! Better get home quick!"
+  else t == 11
+    puts "Time to apparate!"
+  end
+end
+complex_curfew_checker(11)
 
 def deluxe_curfew_checker(t)
   if t < 11
